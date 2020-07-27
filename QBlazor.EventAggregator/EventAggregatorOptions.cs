6a650a031d8cea01817467ec4a7cf1e1dba81e6a
dotnetnoobie <dotnetnoobie@gmail.com>
2020-07-27 18:36:05 +1000

    Update EventAggregatorOptions.cs

--- QBlazor.EventAggregator/EventAggregatorOptions.cs
+++ QBlazor.EventAggregator/EventAggregatorOptions.cs
@@ -3,8 +3,7 @@
     public class EventAggregatorOptions
     {
         /// <summary>
-        /// If true, Event Aggregator tries to run ComponentBase.StateHasChanged for the target component after
-        /// it has handled the message.
+        /// If true, Event Aggregator tries to run ComponentBase.StateHasChanged for the target component after it has handled the message.
         /// </summary>
         public bool AutoRefresh { get; set; }
     }
